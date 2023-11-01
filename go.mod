e11af6c5672fb96c945a72126c5fb0ca86c18001
linbergil <smyshlyaev.il@yandex.ru>
2023-11-01 03:41:17 +0300

    add string verifying w/ regex

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module unpackstring
 
 go 1.19
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/gijsbers/go-pcre v0.0.0-20161214203829-a84f3096ab3c
+	github.com/stretchr/testify v1.7.0
+)
 
 require (
+	github.com/GRbit/go-pcre v1.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- unpack.go
+++ unpack.go
@@ -2,12 +2,21 @@ package unpackstring
 
 import (
 	"errors"
+	"github.com/GRbit/go-pcre"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(str string) (string, error) {
 	// Place your code here.
-	// test
+
+	re, _ := pcre.Compile(`^\d|\d{2,}|\\(?=\D)|(?=\\)`, pcre.UTF8)
+
+	res := re.MatchStringWFlags(str, pcre.UTF8)
+
+	if res == true {
+		return "", ErrInvalidString
+	}
+
 	return "", nil
 }
