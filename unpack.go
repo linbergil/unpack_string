db684c6ae720e0acb62b7cb3cfa2dda09e8b335e
linbergil <smyshlyaev.il@yandex.ru>
2023-11-01 23:05:25 +0300

    delete PCRE regex

--- unpack.go
+++ unpack.go
@@ -2,21 +2,28 @@ package unpackstring
 
 import (
 	"errors"
-	"github.com/GRbit/go-pcre"
+	"regexp"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	// Place your code here.
-
-	re, _ := pcre.Compile(`^\d|\d{2,}|\\(?=\D)|(?=\\)`, pcre.UTF8)
-
-	res := re.MatchStringWFlags(str, pcre.UTF8)
-
-	if res == true {
-		return "", ErrInvalidString
+	err := CheckString(str)
+	if err != nil {
+		return "", err
 	}
 
 	return "", nil
+}
+
+func CheckString(str string) error {
+
+	re, _ := regexp.Compile(`^\d|\d{2,}`)
+
+	if re.MatchString(str) == true {
+		return ErrInvalidString
+	}
+
+	return nil
 }
